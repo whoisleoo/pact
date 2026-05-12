7e7110f4ccc638484241d7134c3064ab20e876d7
Matheus-Ayres <matheussayres9@gmail.com>
2026-05-11 22:19:35 -0300

    Estruturando o front :building_construction:

---
+++ Front/scripts/tailwind-config.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: "#1e40af",
+                secondary: "#9333ea",
+                accent: "#22c55e"
+            }
+        }
+    }
+}
