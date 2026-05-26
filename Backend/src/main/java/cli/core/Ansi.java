7cf291dbdcdc83d80bea660bcfa6cd78c6417ad1
Bernardo <leoaaa96@gmail.com>
2026-05-26 13:33:45 -0300

    feat: screen header creator

--- Backend/src/main/java/cli/core/Ansi.java
+++ Backend/src/main/java/cli/core/Ansi.java
@@ -9,15 +9,30 @@ public final class Ansi {
     public static final String RESET = "\033[0m";
     public static final String BOLD = "\033[1m";
     public static final String DIM = "\033[2m";
-    public static final String RED = "\033[31m";
-    public static final String GREEN = "\033[32m";
-    public static final String YELLOW = "\033[33m";
-    public static final String CYAN = "\033[36m";
     public static final String CLEAR = "\033[2J\033[H";
+
+    public static final String RED = "\033[31m"; // ERROR
+    public static final String GREEN = "\033[32m"; // SUCESS
+    public static final String YELLOW = "\033[33m"; // WARNING
+
+    public static final String RED_PASTEL = "\033[38;5;210m";
+    public static final String WHITE_PASTEL = "\033[38;5;255m";
+
+
+
+    public static String rgb(int r, int g, int b){
+        return "\033[38;2;" + r + ";" + g + ";" + b + "m";
+    }
 
     public static String bold(String s){
         return BOLD + s + RESET;
     }
+
+    public static String dim(String s){
+        return DIM  + s + RESET;
+    }
+
+
 
     public static String red(String s){
         return RED + s + RESET;
@@ -31,9 +46,14 @@ public final class Ansi {
         return YELLOW + s + RESET;
     }
 
-    public static String cyan(String s){
-        return CYAN + s + RESET;
+    public static String pastel(String s){
+        return RED_PASTEL + s + RESET;
     }
+
+    public static String soft(String s){
+        return WHITE_PASTEL + s + RESET;
+    }
+
 
     private Ansi(){};
 }
--- Backend/src/main/java/cli/core/Screen.java
+++ Backend/src/main/java/cli/core/Screen.java
@@ -1,5 +1,20 @@
 package cli.core;
 //IMPRIME COMPONENTES VISUAIS HEADER BOX E ERROS
 
-public class Screen {
+public final class Screen {
+    private static final int width = 42;
+    private static final String border = "═".repeat(width);
+
+    public static void clearScreen(){
+        System.out.print(Ansi.CLEAR);
+        System.out.flush();
+    }
+
+    public static void header(String titulo){
+        System.out.println(Ansi.RED_PASTEL + "╔" + border + "╗");
+        System.out.println("║" + Ansi.pastel(titulo) + "║");
+        System.out.println("╚" + border + "╝" + Ansi.RESET);
+
+
+    }
 }
