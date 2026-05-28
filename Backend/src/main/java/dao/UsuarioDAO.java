a74a522f9c99fd8d507935a21e1312dac3f9b350
dutsp <eduardo260622@gmail.com>
2026-05-28 20:56:16 -0300

    foi UsuarioDAO findById implementado

--- Backend/src/main/java/dao/UsuarioDAO.java
+++ Backend/src/main/java/dao/UsuarioDAO.java
@@ -6,6 +6,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
+import model.domain.Autenticacao;
+import model.domain.Email;
+import model.domain.Nome;
 import model.repositories.Usuario;
 
 public class UsuarioDAO {
@@ -17,12 +21,28 @@ public class UsuarioDAO {
     }
 
     public void insert(Usuario usuario) throws SQLException {
-
+        
     }
 
     public Usuario findById(Long id) throws SQLException {
-        return null;
+    String sql = "select id_usuario, nome, e_mail, created_at from usuario where id_usuario = ?";
+
+    try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+        stmt.setLong(1, id);
+
+        try (ResultSet rs = stmt.executeQuery()) {
+            if (rs.next()) {
+                return new Usuario(
+                    rs.getLong("id_usuario"),
+                    new Nome(rs.getString("nome")),
+                    new Autenticacao(new Email(rs.getString("e_mail")), null),
+                    rs.getTimestamp("created_at").toLocalDateTime()
+                );
+            }
+        }
     }
+    return null;
+}
 
     public Usuario findByEmail(String email) throws SQLException {
         return null;
