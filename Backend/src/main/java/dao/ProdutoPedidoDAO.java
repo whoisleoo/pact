d5792e01bcda2273dd57ce7e20a9273ae8e388b0
niyjn <engs-gabrielschafranski@camporeal.edu.br>
2026-05-29 15:51:42 -0300

    feat: add ProdutoPedidoDAO functions

--- Backend/src/main/java/dao/ProdutoPedidoDAO.java
+++ Backend/src/main/java/dao/ProdutoPedidoDAO.java
@@ -6,6 +6,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import model.domain.Preco;
+import model.domain.Quantidade;
 import model.repositories.ProdutoPedido;
 
 public class ProdutoPedidoDAO {
@@ -16,9 +18,46 @@ public class ProdutoPedidoDAO {
         this.connection = connection;
     }
 
-    public void insertBatch(List<ProdutoPedido> itens) throws SQLException {}
+    public void insertBatch(List<ProdutoPedido> itens) throws SQLException {
+        String sql =
+            "INSERT INTO produto_pedido (id_pedido, id_produto, quantidade, preco_unitario) VALUES (?, ?, ?, ?)";
+
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            for (ProdutoPedido item : itens) {
+                stmt.setLong(1, item.idPedido());
+                stmt.setLong(2, item.idProduto());
+                stmt.setInt(3, item.getQuantidade());
+                stmt.setBigDecimal(4, item.precoUnitario().getValor());
+
+                stmt.addBatch();
+            }
+
+            stmt.executeBatch();
+        }
+    }
 
     public List<ProdutoPedido> findByPedido(Long idPedido) throws SQLException {
-        return new ArrayList<>();
+        String sql = "select * from produto_pedido where id_pedido = ?";
+        List<ProdutoPedido> produtos = new ArrayList<>();
+
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setLong(1, idPedido);
+
+            try (ResultSet rs = stmt.executeQuery()) {
+                while (rs.next()) {
+                    produtos.add(
+                        new ProdutoPedido(
+                            rs.getLong("id_produto"),
+                            rs.getLong("id_pedido"),
+                            rs.getLong("id_produto_pedido"),
+                            new Preco(rs.getBigDecimal("preco_unitario")),
+                            new Quantidade(rs.getInt("quantidade"))
+                        )
+                    );
+                }
+            }
+        }
+
+        return produtos;
     }
 }
